7fccb33211091f28e58061bf5ac146618e16a707
Mike <mike.chen@joocatech.com>
2026-03-10 04:34:42 +0800

    Initial Commit

---
+++ AILocalizationTextPOC/AILocalizationTextPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AILocalizationTextPOCApp.swift
+//  AILocalizationTextPOC
+//
+//  Created by Mike Chen on 2026/3/10.
+//
+
+import SwiftUI
+
+@main
+struct AILocalizationTextPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AILocalizationTextPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AILocalizationTextPOC
+//
+//  Created by Mike Chen on 2026/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
